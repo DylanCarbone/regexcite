bb36e428aa9e4b8845035a9485b2f04b2675bfa9
DylanCarbone <dylcar@ceh.ac.uk>
2023-12-13 11:21:26 +0000

    first commit

--- package creation.R
+++ package creation.R
@@ -3,4 +3,12 @@ packageVersion("devtools")
 
 #create_package("regexcite")
 
+# Create a git pane
 use_git()
+
+# Create a function named strsplit1
+use_r("strsplit1")
+
+# Loads all functions
+load_all()
+
